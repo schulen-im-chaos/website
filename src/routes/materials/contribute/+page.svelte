10679b8a3a63a9eca4867327f9d71e413a0d6286
alexcoder04 <alexcoder04@protonmail.com>
2024-09-25 17:04:40 +0200

    note about pdf on contribute page

--- src/routes/materials/contribute/+page.svelte
+++ src/routes/materials/contribute/+page.svelte
@@ -14,7 +14,7 @@
 	können. Bis dahin schreibe uns bitte eine E-Mail (<a
 		class="anchor"
 		href="mailto:kontakt@schulenimchaos.de">kontakt@schulenimchaos.de</a
-	>), zu der du die Dateien anhängst und Folgendes angibst:
+	>) und gib Folgendes an:
 </p>
 <ul class="list">
 	<li>
@@ -47,9 +47,15 @@
 		>
 		<span class="flex-auto">Sprache (vorallem bei Bilingualen Inhalten relevant)</span>
 	</li>
+	<li>
+		<span class="badge-icon p-2 variant-soft-primary"
+			><i class="fa-solid fa-chevron-right"></i></span
+		>
+		<span class="flex-auto">Die Datei selbst im PDF-Format (zur Not geht auch JPG/PNG, aber bitte kein Word)</span>
+	</li>
 </ul>
 <p>
 	Bitte beachte, dass die Inhalte nicht copyright-geschützt sein dürfen, sie müssen also entweder
 	von dir selbst erstellt oder in public domain verfügbar sein.
 </p>
-<p>Danke für Deinen Beitrag!</p>
+<p>Vielen Dank für Deinen Beitrag zum Projekt!</p>
